9e298dfa2e1713fc5b3651f3ebcf3dd123762a0d
quiddler <>
2018-05-28 16:11:31 -0400

    adaline

---
+++ adaline.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+################################################################################################
+#                                                                                        Adaline
+class Adaline(object):
+    ''' Adaptive Linear Neuron Classifier
+
+    Paramateres
+    -----------
+
+    eta : float
+        Learning rate (between 0.0 and 1.0)
+
+    n_iter : int
+        Passes over the training dataset
+
+    Attributes
+    ----------
+
+    w_ : 1D array
+        Weights after fitting
+
+    errors_ : list
+        Number of misclassifications in every epoch
+
+    '''
+
+    def __init__(self, eta = 0.01, n_iter = 50):
+        self.eta = eta
+        self.n_iter = n_iter
+    
+    def net_input(self, X):
+        # calculate net input
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, X):
+        # compute linear activation
+        return self.net_input(X)
+
+    def predict(self, X):
+        # return class label after unit step function
+        return np.where(self.activation(X) >= 0.0, 1, -1)
+
+    def fit(self, X, y):
+        ''' Fit training data
+
+        Paramaters
+        ----------
+
+        X : {array-like}, shape = [n_samples, n_features]
+            Training vectors,
+            where n_samples is the number of samples
+            and n_features is the number of features
+
+        y : array-like, shape = [n_samples]
+            Target values
+
+        Returns
+        -------
+
+        self : object
+        '''
+
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            output = self.net_input(X)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors ** 2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+
+################################################################################################
+#                                                                                           main
+if __name__ == "__main__":
+
+    # original data for petal and sepal lengths (shows they are linearly seperable)
+    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+
+    y = df.iloc[0:100, 4].values
+    y = np.where(y == 'Iris-setosa', -1, 1)
+    X = df.iloc[0:100, [0, 2]].values
+
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+    ada1 = Adaline(n_iter=10, eta=0.01).fit(X, y)
+
+    ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
+    ax[0].set_xlabel('Epochs')
+    ax[0].set_ylabel('log(Sum Squared Error)')
+    ax[0].set_title('Adaline - learning rate 0.01')
+
+    ada2 = Adaline(n_iter=10, eta=0.0001).fit(X, y)
+
+    ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
+    ax[1].set_xlabel('Epochs')
+    ax[1].set_ylabel('Sum Squared Error')
+    ax[1].set_title('Adaline - learning rate 0.0001')
+
+    plt.show()
