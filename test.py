c6187e12f98c2976f8ed42f58054a6318d19eaef
quiddler <>
2018-05-29 13:58:47 -0400

    sgd added to adaline

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
-import sys
-import os
-import pandas as pd 
-import numpy as np
+import perceptron as ppn
 
-def main():
-    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
-    return df
+p = ppn()
 
-if __name__ == "__main__":
-    df = main()
-    y = df.iloc[0:100, 4].values
-    print(y)
-    y = np.where(y == 'Iris-setosa', -1, 1)
-    print(y)
+print(p)
